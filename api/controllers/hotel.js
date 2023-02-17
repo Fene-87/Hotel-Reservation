92f88fccd10d26c7066a6b4a313350802fe2b3f0
Fene-87 <fenekayas87@gmail.com>
2023-02-17 23:18:32 +0300

    Refactor UI to get property count from the database

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -38,11 +38,9 @@ export const getHotel = async (req, res, next) => {
     }
 }
 
-export const getHotels = async (req, res, next) => {
-    const failed = true;
-    
+export const getHotels = async (req, res, next) => { 
     try {
-        const hotels = await Hotel.find();
+        const hotels = await Hotel.find(req.query);
         res.status(200).json(hotels);
     } catch (err) {
         next(err);
@@ -57,6 +55,26 @@ export const countByCity = async (req, res, next) => {
             return Hotel.countDocuments({city:city});
         }))
         res.status(200).json(list);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const countByType = async (req, res, next) => { 
+    try {
+        const hotelCount = await Hotel.countDocuments({type:"Hotel"});
+        const apartmentCount = await Hotel.countDocuments({type:"apartment"});
+        const resortCount = await Hotel.countDocuments({type:"resort"});
+        const villaCount = await Hotel.countDocuments({type:"villa"});
+        const cabinCount = await Hotel.countDocuments({type:"cabin"});
+
+        res.status(200).json([
+            {type:"Hotels", count:hotelCount},
+            {type:"Apartments", count:apartmentCount},
+            {type:"Resorts", count:resortCount},
+            {type:"Villas", count:villaCount},
+            {type:"Cabins", count:cabinCount}
+        ]);
     } catch (err) {
         next(err);
     }
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
@@ -20,6 +20,6 @@ router.get('/find/:id', getHotel);
 //GET ALL
 router.get('/', getHotels);
 router.get('/countByCity', countByCity);
-router.get('/countByType', getHotels);
+router.get('/countByType', countByType);
 
 export default router;
